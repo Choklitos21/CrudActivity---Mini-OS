f56f79aed35e046a03bdb2484609373a51b89a98
saintdev00 <jqydjdj@gmail.com>
2025-12-05 17:51:13 -0500

    Se le agregaron botones distintos,opciones,se le puso el input,se agrego js para actualizar la pagina y se le agrego hover de opciones a la opcion favoritos

---
+++ apps/script.js
@@ -0,0 +1,7 @@
+const botonRecargar = document.getElementById('btn-recargar');
+
+botonRecargar.addEventListener('click', function() {
+location.reload();
+});
+
+// alert("JavaScript esta conectado");
