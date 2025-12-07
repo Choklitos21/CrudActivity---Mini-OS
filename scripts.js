e3b2ea9edbd91d0ed73a31ff831ee361614773be
Choklitos21 <diego.mm09@hotmail.com>
2025-12-07 14:51:35 -0500

    added music and finder apps to desktop

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
 
-
-const app3 = document.getElementById('miBoton');
-
 const appsOnDisplay = document.getElementById('appsDisplay');
 
 // Show current time and date
@@ -30,12 +27,12 @@ function updateClock() {
 updateClock();
 setInterval(updateClock, 1000);
 
-const finderButtons = document.querySelectorAll('.finderButton');
+const appsButtons = document.querySelectorAll('.appsButton');
 
-finderButtons.forEach(button => {
+appsButtons.forEach(button => {
     button.addEventListener('click',  function toggleIframe() {
         // Obtenemos el elemento iframe y el botón
-        var iframeFinder = document.getElementById('iframeShow');
+        var iframeFinder = document.getElementById('appsShow');
 
         console.log(iframeFinder)
         if (iframeFinder) {
@@ -45,13 +42,13 @@ finderButtons.forEach(button => {
             const iframeFinder = document.createElement('iframe');
 
             iframeFinder.classList.add('iframeStyles');
-            iframeFinder.setAttribute("id", "iframeShow");
+            iframeFinder.setAttribute("id", "appsShow");
 
             iframeFinder.src = 'apps/cesar/apps.html'; // URL a mostrar
             iframeFinder.width = '100%'; // Ancho completo del div
             iframeFinder.height = '100%'; // Alto completo del div
             iframeFinder.frameBorder = '0'; // Sin borde
-            iframeFinder.title = 'App Finder';
+            iframeFinder.title = 'Apps';
 
             appsOnDisplay.appendChild(iframeFinder);
 
@@ -90,3 +87,66 @@ terminalButtons.forEach(button => {
 
     });
 })
+
+// For the finder
+const finderButtons = document.querySelectorAll('.finderButton');
+
+finderButtons.forEach(button => {
+    button.addEventListener('click', function toggleIframe() {
+        // Obtenemos el elemento iframe y el botón
+        var iframeTerminal = document.getElementById('finderShow');
+
+        console.log(iframeTerminal)
+        if (iframeTerminal) {
+            appsOnDisplay.removeChild(iframeTerminal);
+        } else {
+
+            const iframeTerminal = document.createElement('iframe');
+
+            iframeTerminal.classList.add('iframeStylesMax');
+            iframeTerminal.setAttribute("id", "finderShow");
+
+            iframeTerminal.src = 'apps/finder/finder.html';
+            iframeTerminal.width = '100%';
+            iframeTerminal.height = '100%';
+            iframeTerminal.frameBorder = '0';
+            iframeTerminal.title = 'App Finder';
+
+            appsOnDisplay.appendChild(iframeTerminal);
+
+        }
+
+    });
+})
+
+// For the music
+const musicButtons = document.querySelectorAll('.musicButton');
+
+musicButtons.forEach(button => {
+    button.addEventListener('click', function toggleIframe() {
+        // Obtenemos el elemento iframe y el botón
+        var iframeTerminal = document.getElementById('musicShow');
+
+        console.log(iframeTerminal)
+        if (iframeTerminal) {
+            appsOnDisplay.removeChild(iframeTerminal);
+        } else {
+
+            const iframeTerminal = document.createElement('iframe');
+
+            iframeTerminal.classList.add('iframeStylesMax');
+            iframeTerminal.setAttribute("id", "musicShow");
+
+            iframeTerminal.src = 'apps/music/music.html';
+            iframeTerminal.width = '100%';
+            iframeTerminal.height = '100%';
+            iframeTerminal.frameBorder = '0';
+            iframeTerminal.title = 'App Music';
+
+            appsOnDisplay.appendChild(iframeTerminal);
+
+        }
+
+    });
+})
+
